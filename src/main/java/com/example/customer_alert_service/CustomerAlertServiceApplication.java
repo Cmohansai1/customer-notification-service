58c6d4574a6ee055783b0f617d3d4b2e6e1d0baf
Mohan <mohansai3577@gmail.com>
2025-06-29 11:32:50 -0400

    Added full Spring Boot project files

---
+++ src/main/java/com/example/customer_alert_service/CustomerAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_alert_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAlertServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customer_alert_service/CustomerAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_alert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
